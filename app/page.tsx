d5865b7647427b33d694cfd658c19e59126333a2
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2025-11-05 15:35:17 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hello from "@/components/Hello";
+
+const Home = () => {
+    console.log('What type of a component am I?')
+    return (
+        <main>
+            <div className="text-5xl underline">Welcome to Next.js</div>
+            <Hello/>
+        </main>
+    )
+}
+export default Home
